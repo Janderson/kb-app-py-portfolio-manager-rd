6bac537f55f89e2f5f3002298cd12e1a6165d636
Janderson <ffjanderson@gmail.com>
2023-03-12 11:20:57 -0300

    <feat> comando para markowitz com leverage

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 @author: Janderson FFerreira
 """
 import click
-from core.markowitz import run_markowitz, run_markowitz_from_data_service
+from core.markowitz import run_markowitz, run_markowitz_from_data_service, run_markowitz_with_leverage_from_data_service
 from datetime import date, timedelta
 
 VERSION = "1.1"
@@ -30,6 +30,26 @@ def cmd_run_markowitz(stocks, start_date: str):
     stocks = stocks.split(",")
     run_markowitz_from_data_service(stocks=stocks, 
                                     start_date=start_date)
+
+
+@cli.command("markowitz_with_leverage")
+@click.argument("stocks", default='BOVA11.SA,SMAL11.SA')
+@click.option("--start-date", "start_date", default=None)
+@click.option("--expected_return", "expected_return", default=None, type=click.FLOAT)
+def cmd_run_markowitz(stocks, start_date: str, expected_return:float):
+    if not start_date:
+        start_date = (date.today() - (timedelta(days=365)*5)).isoformat()
+    else:
+        start_date = start_date
+    print(f"SIMULADOR MARKOWITZ  feito por Janderson FFerreira v{VERSION}\n\n")
+    print(f"ativos: {stocks}")
+    print(f"data inicio: {start_date}")
+    print(f"data fim: {date.today()}")
+
+    stocks = stocks.split(",")
+    run_markowitz_with_leverage_from_data_service(stocks=stocks, 
+                                                  start_date=start_date,
+                                                  expected_return=expected_return)
 
 
 @cli.command("download_yfinance")
