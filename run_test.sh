befe8b1db692f0b080f82cdf458d53c8ddb5cd86
Janderson <ffjanderson@gmail.com>
2023-03-12 11:22:26 -0300

    <desenv> atualizado comando de testes

--- run_test.sh
+++ run_test.sh
@@ -1,1 +1,6 @@
-python -m unittest
+# python -m unittest
+python -m unittest tests/core/test_markowitz.py
+python -m unittest tests/core/test_markowitz_leverage_ajust.py
+
+python -m unittest tests/core/test_data_service.py
+python -m unittest tests/core/test_cdataframe.py
